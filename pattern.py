662ae4d73cc84af98966cbd4393d2030c5809d7b
Poisonlam <fi2laiyanyu@126.com>
2018-05-11 16:38:31 +0800

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,11 +1,11 @@
 from solve import *
 
 patternList = [
-    ['action', '肌肉']
-    ['action', 'muscle']
-    ['action', '有效']
-    ['可以', 'muscle']  # 怎么样可以锻炼腹肌
-    ['action', 'special']  # 平板支撑需要什么器械/道具/设备/条件
+    ['action', '肌肉'],
+    ['action', 'muscle'],
+    ['action', '有效'],
+    ['可以', 'muscle'],  # 怎么样可以锻炼腹肌
+    ['action', 'special'],  # 平板支撑需要什么器械/道具/设备/条件
     ['aciton', '动作']  # 跟仰卧起坐差不多的动作有什么
 ]
 
@@ -44,15 +44,15 @@ def pattern(sentence):
             if index == 0:
                 return get_muscle_of_action(sentence[0][1])
             if index == 1:
-                return ##捕捉肌肉关系库
+                return get_muscleGroup_action(sentence[1][1])
             if index == 2:
                 return get_details_of_action(sentence[0][1])
             if index == 3:
-                return #action of muscle
+                return get_muscleGroup_action(sentence[1][1])
             if index == 4:
                 return get_equipment_of_action(sentence[0][1])
             if index == 5:
-                return #action of muscle
+                return get_muscleGroup_action(sentence[0][1])
     return 'unknown'
 
 
