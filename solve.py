36baae7922969dfaac1a4567f0c840e147cb7c65
Poisonlam <fi2laiyanyu@126.com>
2018-05-17 21:41:47 +0800

    Update solve.py
    增加
    1 通过肌肉查找动作
    2 通过肌肉查找器械
    3 通过器械查找动作

--- solve.py
+++ solve.py
@@ -43,7 +43,17 @@ def get_equipmentlist_of_muscle(muscle):
     # 返回肌肉相关器械
     return MuscleGroup(muscle).find_related_equipments()
 
+def find_action_of_euqipments(equipment):
+    res = Mongo.action.find()
+    action_list = []
+    for i in res:
+        if i['equipment'] == equipment:
+            if i['name'] not in action_list:
+                action_list.append(i['name'])
+    return action_list
 if __name__ == '__main__':
     # print(get_muscle_of_action('平板支撑'))
     # print(get_muscleGroup_action('肱二头肌'))
     print(get_equipmentlist_of_muscle('腹直肌'))
+    # print(get_actionlist_of_muscle('腹直肌'))
+    # print(find_action_of_euqipments('弹力带'))
