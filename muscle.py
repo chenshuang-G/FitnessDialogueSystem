824e93320a5175710f5cde6ab01d2ca8d8c8ce49
Poisonlam <fi2laiyanyu@126.com>
2018-05-17 21:30:11 +0800

    Update muscle.py
    加了 根据肌肉返回肌肉相关动作，相关器械的函数

--- muscle.py
+++ muscle.py
@@ -16,6 +16,7 @@ class Muscle:
         return ret
 
 
+
 class MuscleGroup:
     def __init__(self, name):
         self.name = name
@@ -25,4 +26,25 @@ class MuscleGroup:
         dict_muscle = res['muscleGroup']
         new_dict = str(dict_muscle).replace(' ','').split(',')
         return new_dict
+
+    def find_related_action(self):
+        res = Mongo.action.find()
+        action_list = []
+
+        for i in res:
+            for k in i['mainMuscle']:
+                if k == self.name:
+                    print(1)
+                    action_list.append(i['name'])
+        return action_list
+
+    def find_related_equipments(self):
+        res = Mongo.action.find()
+        action_list = []
+        equipment_list = []
+        for i in res:
+            if i['mainMuscle'] == self.name:
+                if i['equipment'] not in equipment_list:
+                    equipment_list.append(i['equipment'])
+        return equipment_list
 
